59581be9bebf8bb92a45733eabf6298165716ee1
Nicolas-alt <a10100105555@gmail.com>
2021-03-16 13:01:02 -0500

    Refactor index.js

---
+++ src/config/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const hbs = require("hbs");
+const connection = require("./dbConfig");
+
+class Server {
+	constructor() {
+		this.app = express();
+		this.port = process.env.PORT;
+		this.server = http.createServer(this.app);
+		connection();
+	}
+
+	middlewares() {
+		this.app.use(cors());
+		this.app.use(express.json());
+		this.app.set("view engine", "hbs");
+		hbs.registerPartials(__dirname + "/components/", function (err) {
+			console.log(err);
+		});
+	}
+
+	routes() {
+		// api
+		this.app.use("/api/v1/auth", require("../routes/authRoutes"));
+
+
+		// application
+	}
+
+	start() {
+		this.middlewares();
+		this.routes();
+		this.server.listen(this.port, () => {
+			console.log("Server running:", this.port);
+		});
+	}
+}
+
+module.exports = Server;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const hbs = require('hbs');
+require("dotenv").config();
+const Server = require("./config/server");
 
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/components/', function (err) {});
-
-app.get('/', (req, res) => {});
-
-app.get('/login', (req, res) => {
-  res.render('login', {
-    name: 'Federico',
-  });
-});
-
-app.get('*', (req, res) => {
-  res.send('404');
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Working on ${process.env.PORT}`);
-});
+const server = new Server();
+server.start();

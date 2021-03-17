517c54159c24b31874ec92f549b6feb2234a8022
Nicolas-alt <a10100105555@gmail.com>
2021-03-17 10:09:27 -0500

    Fix header

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
 	res.render("index", {
+		title: "Instagram Clone",
 		user: {
 			authenticated: true
 		}
@@ -10,15 +11,19 @@ router.get("/", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
-	res.render("layouts/login");
+	res.render("layouts/login", {
+		title: "Login"
+	});
 });
 
 router.get("/register", (req, res) => {
-	res.render("layouts/register");
+	res.render("layouts/register", { title: "Register" });
 });
 
 router.get("/profile", (req, res) => {
-	res.render("layouts/profile");
+	res.render("layouts/profile", {
+		title: "Nicolas Jimenez | Instagram",
+	});
 });
 
 module.exports = router;

5d7a44db7971d29b3e258cfcf29bb69d7248f8d6
Nicolas-alt <a10100105555@gmail.com>
2021-03-17 07:39:48 -0500

    Add basic layout

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	res.render("index");
+	res.render("index", {
+		user: {
+			authenticated: true
+		}
+	});
 });
 
 router.get("/login", (req, res) => {

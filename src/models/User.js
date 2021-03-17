e2dc04ff4afb0e533b69c7f2b98c3f3ed9c3ff22
Nicolas-alt <a10100105555@gmail.com>
2021-03-17 09:01:13 -0500

    Add password methods

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,11 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserChema = new Schema({
+	realName: {
+		type: String,
+		required: true,
+	},
 	userName: {
 		type: String,
 		required: true,
@@ -14,5 +19,14 @@ const UserChema = new Schema({
 		required: true,
 	},
 });
+
+UserChema.methods.encryptPassword = async function (password) {
+	const salt = await bcrypt.getSalt(15);
+	return await bcrypt.hash(password, salt);
+};
+
+UserChema.methods.decryptPassword = async function (password) {
+	return await bcrypt.compare(password, this.password);
+};
 
 module.exports = model("User", UserChema);
